d12dbaf62b039d8095ce18db21443a7ae181fc84
Chyconn <65868625+Chyconn@users.noreply.github.com>
2020-05-26 14:15:21 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,20 +11,6 @@ class Bot:
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver')
 
     def SendMessage(self):
-        self.driver.get('https://web.whatsapp.com')
-        time.sleep(30)
-        group = self.driver.find_element_by_xpath(f"//span[@title='Renata Marques']")
-        time.sleep(3)
-        group.click()
-        chat_box = self.driver.find_element_by_class_name('_13mgZ')
-        time.sleep(3)
-        chat_box.click()
-        chat_box.send_keys(self.message)
-        send_button = self.driver.find_element_by_xpath("//span[@data-icon='send']")
-        time.sleep(3)
-        send_button.click()
-        time.sleep(3)
-
-wbot = Bot()
-wbot.SendMessage()
+        self.driver.get('https://www.promobank.com.br/')
+        
 
