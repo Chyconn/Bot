0c6b6c3946e33c8c5a7222c6748e5dda38c9a705
Chyconn <65868625+Chyconn@users.noreply.github.com>
2020-05-26 14:44:20 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import time
 
 class Bot:
     def __init__(self):
-        self.message = "O valor disponível na caixinha é x"
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver')
